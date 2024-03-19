f47afecc1b7a4970763c0a7829761f79116ed30f
Lixander <lixanderhilariolopez@gmail.com>
2024-03-18 23:41:37 -0400

    commmit changes

---
+++ src/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea Docker Compose</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+    <h1 >Tarea de Docker Compose del maestro Elvys Cruz para Electiva 2</h1>
+
+<?php
+        echo "<h2>Lixander Ysael Hilario Lopez</h2>";
+        echo "<h2>2021-1855</h2>";
+    ?>
+    
+</body>
+</html>
